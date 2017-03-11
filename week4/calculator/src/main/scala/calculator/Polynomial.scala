125aaf411de8fb8f056caec8b77864e5f8e40aef
alibin <qlibin@gmail.com>
2017-03-11 19:59:20 +0300

    warm-up part completed

--- week4/calculator/src/main/scala/calculator/Polynomial.scala
+++ week4/calculator/src/main/scala/calculator/Polynomial.scala
@@ -1,13 +1,23 @@
 package calculator
 
 object Polynomial {
-  def computeDelta(a: Signal[Double], b: Signal[Double],
-      c: Signal[Double]): Signal[Double] = {
-    ???
+  def computeDelta(a: Signal[Double],
+                   b: Signal[Double],
+                   c: Signal[Double]): Signal[Double] = Signal {
+    val bValue = b()
+    bValue * bValue - 4 * a() * c()
   }
 
-  def computeSolutions(a: Signal[Double], b: Signal[Double],
-      c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
-    ???
+  def computeSolutions(a: Signal[Double],
+                       b: Signal[Double],
+                       c: Signal[Double],
+                       delta: Signal[Double]): Signal[Set[Double]] = Signal {
+    delta() match {
+      case d if d < 0 => Set()
+      case d => Set(
+        (-b() + Math.sqrt(d)) / 2 * a(),
+        (-b() - Math.sqrt(d)) / 2 * a()
+      )
+    }
   }
 }
