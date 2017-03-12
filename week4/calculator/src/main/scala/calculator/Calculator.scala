a646165305223e1c510ae6fed9388f6413e80e5a
alibin <qlibin@gmail.com>
2017-03-12 18:54:55 +0300

    completed calculator assignment

--- week4/calculator/src/main/scala/calculator/Calculator.scala
+++ week4/calculator/src/main/scala/calculator/Calculator.scala
@@ -9,20 +9,35 @@ final case class Times(a: Expr, b: Expr) extends Expr
 final case class Divide(a: Expr, b: Expr) extends Expr
 
 object Calculator {
-  def computeValues(
-      namedExpressions: Map[String, Signal[Expr]]): Map[String, Signal[Double]] = {
-    ???
-  }
+  def computeValues(namedExpressions: Map[String, Signal[Expr]]): Map[String, Signal[Double]] =
+    namedExpressions.map {
+      case (name, expr) => (name, Signal(eval(getReferenceExpr(name, namedExpressions), namedExpressions)))
+    }
 
-  def eval(expr: Expr, references: Map[String, Signal[Expr]]): Double = {
-    ???
-  }
+  def eval(expr: Expr, references: Map[String, Signal[Expr]]): Double =
+    eval_(expr, references, Set())
+
+  private def eval_(expr: Expr, references: Map[String, Signal[Expr]], evaluated: Set[Expr]): Double =
+    if (evaluated.contains(expr)) Double.NaN
+    else expr match {
+      case Literal(value) => value
+      case Ref(name) => val refExpr = getReferenceExpr(name, references)
+          eval_(refExpr, references, evaluated + expr)
+      case Plus(a, b) =>
+        eval_(a, references, evaluated) + eval_(b, references, evaluated)
+      case Minus(a, b) =>
+        eval_(a, references, evaluated) - eval_(b, references, evaluated)
+      case Times(a, b) =>
+        eval_(a, references, evaluated) * eval_(b, references, evaluated)
+      case Divide(a, b) =>
+        eval_(a, references, evaluated) / eval_(b, references, evaluated)
+    }
 
   /** Get the Expr for a referenced variables.
    *  If the variable is not known, returns a literal NaN.
    */
   private def getReferenceExpr(name: String,
-      references: Map[String, Signal[Expr]]) = {
+      references: Map[String, Signal[Expr]]): Expr = {
     references.get(name).fold[Expr] {
       Literal(Double.NaN)
     } { exprSignal =>
--- week4/calculator/src/test/scala/calculator/CalculatorSuite.scala
+++ week4/calculator/src/test/scala/calculator/CalculatorSuite.scala
@@ -1,16 +1,16 @@
 package calculator
 
-import org.scalatest.FunSuite
-
+import calculator.TweetLength.MaxTweetLength
 import org.junit.runner.RunWith
+import org.scalatest.{FunSuite, _}
+import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.junit.JUnitRunner
 
-import org.scalatest._
-
-import TweetLength.MaxTweetLength
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 
 @RunWith(classOf[JUnitRunner])
-class CalculatorSuite extends FunSuite with ShouldMatchers {
+class CalculatorSuite extends FunSuite with ShouldMatchers with ScalaFutures {
 
   /******************
    ** TWEET LENGTH **
@@ -49,6 +49,199 @@ class CalculatorSuite extends FunSuite with ShouldMatchers {
     assert(resultRed1() == "red")
     val resultRed2 = TweetLength.colorForRemainingCharsCount(Var(-5))
     assert(resultRed2() == "red")
+  }
+
+  test("self ref detection") {
+    val namedExpressions: Map[String, Signal[Expr]] = Map(
+      "a" -> Signal(Ref("a"))
+    )
+
+    val futureTest = Future {
+      val namedValues: Map[String, Signal[Double]] =
+        Calculator.computeValues(namedExpressions)
+      val aSignal: Signal[Double] = namedValues("a")
+      namedValues("a")()
+    }
+
+    assert(futureTest.futureValue.isNaN)
+  }
+
+  test("simple 2x loop detection") {
+    val namedExpressions: Map[String, Signal[Expr]] = Map(
+      "a" -> Signal(Ref("b")),
+      "b" -> Signal(Ref("a"))
+    )
+
+    val futureTest = Future {
+      val namedValues: Map[String, Signal[Double]] =
+        Calculator.computeValues(namedExpressions)
+      val aSignal: Signal[Double] = namedValues("a")
+      namedValues("a")()
+    }
+
+    assert(futureTest.futureValue.isNaN)
+  }
+
+  test("simple 4x loop detection") {
+    val namedExpressions: Map[String, Signal[Expr]] = Map(
+      "a" -> Signal(Ref("b")),
+      "b" -> Signal(Ref("c")),
+      "c" -> Signal(Ref("d")),
+      "d" -> Signal(Ref("a"))
+    )
+
+    val futureTest = Future {
+      val namedValues: Map[String, Signal[Double]] =
+        Calculator.computeValues(namedExpressions)
+      val aSignal: Signal[Double] = namedValues("a")
+      namedValues("a")()
+    }
+
+    assert(futureTest.futureValue.isNaN)
+  }
+
+  test("complex self ref detection") {
+    val namedExpressions: Map[String, Signal[Expr]] = Map(
+      "a" -> Signal(Plus(Ref("a"), Literal(1)))
+    )
+
+    val futureTest = Future {
+      val namedValues: Map[String, Signal[Double]] =
+        Calculator.computeValues(namedExpressions)
+      val aSignal: Signal[Double] = namedValues("a")
+      namedValues("a")()
+    }
+
+    assert(futureTest.futureValue.isNaN)
+  }
+
+  test("complex 2x loop detection") {
+    val namedExpressions: Map[String, Signal[Expr]] = Map(
+      "a" -> Signal(Plus(Ref("b"), Literal(1))),
+      "b" -> Signal(Plus(Ref("a"), Literal(1)))
+    )
+
+    val futureTest = Future {
+      val namedValues: Map[String, Signal[Double]] =
+        Calculator.computeValues(namedExpressions)
+      val aSignal: Signal[Double] = namedValues("a")
+      namedValues("a")()
+    }
+
+    assert(futureTest.futureValue.isNaN)
+  }
+
+  test("complex 4x loop detection") {
+    val namedExpressions: Map[String, Signal[Expr]] = Map(
+      "a" -> Signal(Plus(Ref("b"), Literal(1))),
+      "b" -> Signal(Plus(Ref("c"), Ref("d"))),
+      "c" -> Signal(Plus(Ref("d"), Literal(1))),
+      "d" -> Signal(Plus(Ref("a"), Literal(1)))
+    )
+
+    val futureTest = Future {
+      val namedValues: Map[String, Signal[Double]] =
+        Calculator.computeValues(namedExpressions)
+      val aSignal: Signal[Double] = namedValues("a")
+      namedValues("a")()
+    }
+
+    assert(futureTest.futureValue.isNaN)
+  }
+
+  test("literal evaluation") {
+    val namedExpressions: Map[String, Signal[Expr]] = Map(
+      "a" -> Signal(Literal(1))
+    )
+
+    val futureTest = Future {
+      val namedValues: Map[String, Signal[Double]] =
+        Calculator.computeValues(namedExpressions)
+      val aSignal: Signal[Double] = namedValues("a")
+      namedValues("a")()
+    }
+
+    assert(futureTest.futureValue === 1)
+  }
+
+  test("transitive literal evaluation") {
+    val namedExpressions: Map[String, Signal[Expr]] = Map(
+      "a" -> Signal(Ref("b")),
+      "b" -> Signal(Literal(1))
+    )
+
+    val futureTest = Future {
+      val namedValues: Map[String, Signal[Double]] =
+        Calculator.computeValues(namedExpressions)
+      val aSignal: Signal[Double] = namedValues("a")
+      namedValues("a")()
+    }
+
+    assert(futureTest.futureValue === 1)
+  }
+
+  test("3x transitive literal evaluation") {
+    val namedExpressions: Map[String, Signal[Expr]] = Map(
+      "a" -> Signal(Ref("b")),
+      "b" -> Signal(Ref("c")),
+      "c" -> Signal(Ref("d")),
+      "d" -> Signal(Literal(1))
+    )
+
+    val futureTest = Future {
+      val namedValues: Map[String, Signal[Double]] =
+        Calculator.computeValues(namedExpressions)
+      val aSignal: Signal[Double] = namedValues("a")
+      namedValues("a")()
+    }
+
+    assert(futureTest.futureValue === 1)
+  }
+
+  test("literal + literal evaluation") {
+    val namedExpressions: Map[String, Signal[Expr]] = Map(
+      "a" -> Signal(Plus(Literal(1), Literal(2)))
+    )
+
+    val futureTest = Future {
+      val namedValues: Map[String, Signal[Double]] =
+        Calculator.computeValues(namedExpressions)
+      val aSignal: Signal[Double] = namedValues("a")
+      namedValues("a")()
+    }
+
+    assert(futureTest.futureValue === 3)
+  }
+
+  test("literal + ref evaluation") {
+    val namedExpressions: Map[String, Signal[Expr]] = Map(
+      "a" -> Signal(Plus(Literal(1), Ref("b"))),
+      "b" -> Signal(Literal(2))
+    )
+
+    val futureTest = Future {
+      val namedValues: Map[String, Signal[Double]] =
+        Calculator.computeValues(namedExpressions)
+      val aSignal: Signal[Double] = namedValues("a")
+      namedValues("a")()
+    }
+
+    assert(futureTest.futureValue === 3)
+  }
+
+  test("ref unknown detection") {
+    val namedExpressions: Map[String, Signal[Expr]] = Map(
+      "a" -> Signal(Plus(Literal(1), Ref("b")))
+    )
+
+    val futureTest = Future {
+      val namedValues: Map[String, Signal[Double]] =
+        Calculator.computeValues(namedExpressions)
+      val aSignal: Signal[Double] = namedValues("a")
+      namedValues("a")()
+    }
+
+    assert(futureTest.futureValue.isNaN)
   }
 
 }
